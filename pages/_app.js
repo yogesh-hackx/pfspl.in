eaa5132549164c7e5c4a8952690ac699ee237924
Yogesh Bhatt <me@yogicodes.com>
2021-02-06 21:04:02 +0530

    update: add meta-theme tag

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,7 @@ function MyApp({ Component, pageProps }) {
                     name="viewport"
                     content="width=device-width, initial-scale=1.0"
                 />
+                <meta name=" theme-color" content="#000" />
             </Head>
 
             <Component {...pageProps} />
