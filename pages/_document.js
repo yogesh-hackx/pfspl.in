a9770c7a607ca887c5841af3757c4a15c091fac5
Yogesh Bhatt <me@yogicodes.com>
2023-06-02 13:52:01 +0530

    fix gtag

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,14 +11,16 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-BMLPDVV17X"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-BMLPDVV17X');
-          </script>
+          <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-BMLPDVV17X" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+  
+              gtag('config', 'G-BMLPDVV17X');
+            `}
+          </Script>
         </Head>
         <body>
           <Main />
