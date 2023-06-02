ca1c918340c5a4824f5adeb0ff7ac59720b66585
Yogesh Bhatt <me@yogicodes.com>
2023-06-02 13:47:53 +0530

    added gtag

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-BMLPDVV17X"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-BMLPDVV17X');
+          </script>
+        </Head>
         <body>
           <Main />
           <NextScript />
